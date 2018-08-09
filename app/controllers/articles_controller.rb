53fc76bf35179f365688a84bf99d40d4f8b06eb7
roncode <ron_benporat@yahoo.com>
2018-08-09 00:27:00 +0000

    DRY articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
+  
   def new
     @article = Article.new
   end
@@ -16,15 +19,12 @@ class ArticlesController < ApplicationController
   
   
   def show
-    @article = Article.find(params[:id])
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = 'Article was successfully updated'
       redirect_to article_path(@article)
@@ -39,7 +39,6 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = 'Article was successfully deleted'
     redirect_to articles_path
@@ -48,5 +47,9 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
